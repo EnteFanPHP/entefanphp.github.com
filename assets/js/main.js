d6b61edf6b2221bb83257321a02354228d7a9f57
EnteFanPHP <78978307+EnteFanPHP@users.noreply.github.com>
2022-03-14 19:16:20 +0100

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var html = document;
+
+function openSidebar(){
+    html.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeSidebar(){
+    document.getElementById("mySidenav").style.width = "0";
+}
