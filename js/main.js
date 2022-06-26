199f1f44826994fa79945c52d4bf16af20c8d2d6
EnteFanPHP <78978307+EnteFanPHP@users.noreply.github.com>
2022-06-26 18:40:39 +0200

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var html = document;
-
-function openSidebar(){
-    html.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeSidebar(){
-    document.getElementById("mySidenav").style.width = "0";
-}
